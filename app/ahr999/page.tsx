f8b124d55468c4612fbba4321ecb4db355ad34d5
kevin <aerockevin@gmail.com>
2024-04-01 15:56:01 +0800

    add fear & greed index

---
+++ app/ahr999/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>ahr999 index</h1>
+        </div>
+    );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'alternative.me',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
