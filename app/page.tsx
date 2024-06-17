747273f608fa93a164bd80605a4c57247a9731d8
kevin <aerockevin@gmail.com>
2024-06-17 16:11:31 +0800

    update f&g

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,6 @@ export default function Home() {
             </div>
             <div style={{display: "flex"}}>
                 {/* See How The Value Goes */}
-                {/*<Image*/}
-                {/*    src="https://alternative.me/crypto/fear-and-greed-index.png"*/}
-                {/*    alt="Latest Crypto Fear & Greed Index"*/}
-                {/*    width={300}*/}
-                {/*    height={300}*/}
-                {/*/>*/}
                 {/*<CoinList/>*/}
                 {/*<Link href="/ahr999">ahr999</Link>*/}
                 {/*<Ahr999Chart/>*/}
@@ -26,14 +20,19 @@ export default function Home() {
                 <Source name={"World Stats"} url={"https://tradingeconomics.com/matrix"}
                         tag={"Index"} description={"World Stats"}/>
                 <Source name={"Stocks"} url={"https://tradingeconomics.com/stocks"}
-                        tag={"Index"} description={"World Main Stocks"}/>
+                        tag={"Index, Stock"} description={"World Main Stocks"}/>
                 <Source name={"Central Bank Rates"} url={"https://en.macromicro.me/central_bank/overview"}
                         tag={"Rate"} description={"Central Bank Rates Overview"}/>
-                <Source name={"Crypto Fear & Greed Index"} url={"https://alternative.me/crypto/fear-and-greed-index"}
-                        tag={"Index"} description={"Crypto Fear & Greed Index"}/>
-                <Source name={"Ahr999 Index"} url={"https://www.coinglass.com/pro/i/ahr999"} tag={"Index"}
-                        description={"Bitcoin Ahr999 Index"}/>
-                <Source name={"Who Runs China"} url={"https://news.cgtn.com/event/2019/whorunschina/index.html"} tag={"graph"} description={"Who Runs China By CGTN"}/>
+                <Source name={"Ahr999 Index"} url={"https://www.coinglass.com/pro/i/ahr999"}
+                        tag={"Index, Crypto"} description={"Bitcoin Ahr999 Index"}/>
+                {/*<Source name={"Crypto Fear & Greed Index"} url={"https://alternative.me/crypto/fear-and-greed-index"}*/}
+                {/*        tag={"Index, Crypto"} description={"Crypto Fear & Greed Index"}/>*/}
+                <Image
+                    src="https://alternative.me/crypto/fear-and-greed-index.png"
+                    alt="Latest Crypto Fear & Greed Index"
+                    width={300}
+                    height={300}
+                />
             </div>
         </main>
     );
