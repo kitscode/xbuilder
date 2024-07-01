1e306df08db6c21640e157b038237c239be37c33
kevin <aerockevin@gmail.com>
2024-07-01 13:54:36 +0800

    add sources 0701

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,12 @@ export default function Home() {
             </div>
             <div style={{display: "flex"}}>
                 {/* See How The Value Goes */}
-                {/*<CoinList/>*/}
-                {/*<Link href="/ahr999">ahr999</Link>*/}
-                {/*<Ahr999Chart/>*/}
+                <Image
+                    src="https://alternative.me/crypto/fear-and-greed-index.png"
+                    alt="Latest Crypto Fear & Greed Index"
+                    width={300}
+                    height={300}
+                />
                 <Source name={"Assets By Market Cap"} url={"https://companiesmarketcap.com/assets-by-market-cap"}
                         tag={"Index"} description={"Assets By Market Cap"}/>
                 <Source name={"World Stats"} url={"https://tradingeconomics.com/matrix"}
@@ -25,14 +28,21 @@ export default function Home() {
                         tag={"Rate"} description={"Central Bank Rates Overview"}/>
                 <Source name={"Ahr999 Index"} url={"https://www.coinglass.com/pro/i/ahr999"}
                         tag={"Index, Crypto"} description={"Bitcoin Ahr999 Index"}/>
-                {/*<Source name={"Crypto Fear & Greed Index"} url={"https://alternative.me/crypto/fear-and-greed-index"}*/}
-                {/*        tag={"Index, Crypto"} description={"Crypto Fear & Greed Index"}/>*/}
-                <Image
-                    src="https://alternative.me/crypto/fear-and-greed-index.png"
-                    alt="Latest Crypto Fear & Greed Index"
-                    width={300}
-                    height={300}
-                />
+                {/*<CoinList/>*/}
+                {/*<Ahr999Chart/>*/}
+                {/*https://twitter.com/drakefjustin/status/1304064879662227456*/}
+            </div>
+            <div style={{display: "flex"}}>
+                <a href="https://ultrasound.money" target="_blank" rel="noopener noreferrer">
+                    <Image
+                        src="/ultrasound.png"
+                        alt="ultrasound money"
+                        width={550}
+                        height={550}
+                    />
+                </a>
+                <Source name={"Gas Price Tracker"} url={"https://cointool.app/gasPrice/eth"} tag={"Index"}
+                        description={"ETH Gas Price Tracker"}/>
             </div>
         </main>
     );
